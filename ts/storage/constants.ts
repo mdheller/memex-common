3b089428cfb5e19f4e1065a1fa4dc069dd3a746f
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-06-12 13:26:31 +0700

    Add tag validity patterns

---
+++ ts/storage/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * Pattern to match hashtag prefix syntax for tags.
+ * NOTE: Please update these together if they change.
+ */
+export const HASH_TAG_PATTERN = /^-?#\w+([-\.]\w+)*$/
+export const VALID_TAG_PATTERN = /^\w+([-\.]\w+)*$/
